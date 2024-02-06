e367f28925eb150186ca62a1653d0e3af4ee90a3
DavidG28 <131462196+DavidG28@users.noreply.github.com>
2024-02-06 12:17:56 +0100

    Update 2_Partición_Externa.py

--- app/pages/2_Partición_Externa.py
+++ app/pages/2_Partición_Externa.py
@@ -4,7 +4,7 @@ import numpy as np
 
 st.set_page_config(layout="wide")
 
-logo_url = './imagenes/logo.png'
+logo_url = 'app/imagenes/logo.png'
 st.sidebar.image(logo_url)
 
 st.title('Partición de Datos Externa (Hold-Out)')
