9e3ca5e805b13d0fb9d83baf9b4024c0eab9f8de
DavidG28 <131462196+DavidG28@users.noreply.github.com>
2024-02-08 23:12:37 +0100

    Update Introducción.py

--- app/Introducción.py
+++ app/Introducción.py
@@ -18,26 +18,26 @@ with tab1:
 
     st.markdown('''## **:orange[Definiciones]**
 
-<div style="text-align: justify; text-justify: inter-word">El primer punto a abordar es definir qué entendemos por crisis financiera, algo que aunque pueda parecer sencillo, debido a todos los matices y escenarios que engloba no resulta una tarea simple. Tras realizar un análisis de la literatura especializada existente al respecto, la mayoría de expertos parecen coincidir en que la definición más adecuada de dicho concepto sería la recogida en el artículo *“Is the crisis problem growing more severe?” (Bordo et al., 2000)* :
+<div style="text-align: justify; text-justify: inter-word">El primer punto a abordar es definir qué entendemos por crisis financiera, algo que aunque pueda parecer sencillo, debido a todos los matices y escenarios que engloba no resulta una tarea simple. Tras realizar un análisis de la literatura especializada existente al respecto, la mayoría de expertos parecen coincidir en que la definición más adecuada de dicho concepto sería la recogida en el artículo <i>“Is the crisis problem growing more severe?” (Bordo et al., 2000)</i>:
 
 #### "Se define crisis financiera como episodios de volatilidad en los mercados financieros marcados por importantes problemas de falta de liquidez e insolvencia entre los distintos actores participantes y/o por falta de intervención oficial/gubernamental para limitar las consecuencias"</div>''', unsafe_allow_html=True)
     
 with tab2:
     st.markdown('''## **:orange[Puntos Importantes]**
     
-<div style="text-align: justify; text-justify: inter-word">Más allá de la definición del concepto, se deberán tener en cuenta los siguientes puntos:
+Más allá de la definición del concepto, se deberán tener en cuenta los siguientes puntos:
 
 + Ámbito geográfico y temporal que se desea contemplar.
 + Abordar ámbitos demasiado amplios podrían llevar a tener problemas para disponer de modelos suficientemente robustos.
 + Diferentes tipos de economías en las que se agrupan los países: economías avanzadas, emergentes, en vías de desarrollo, etc.
 + Diversidad de factores desencadenantes de este tipo de situaciones: normalmente observaremos una conjunción de varios de ellos y no un causante único.
-+ Factores cambiantes a lo largo de la historia y entre los distintos tipos de economías: las causas o indicadores de crisis para un determinado periodo temporal o tipo de economía pueden no ser válidas para otros.</div>''', unsafe_allow_html=True)
++ Factores cambiantes a lo largo de la historia y entre los distintos tipos de economías: las causas o indicadores de crisis para un determinado periodo temporal o tipo de economía pueden no ser válidas para otros.''')
 
 with tab3:
 
     st.markdown('''## **:orange[Problemáticas]**
     
-<div style="text-align: justify; text-justify: inter-word">Algunos de los problemas que han sido necesario abordar a lo largo del desarrollo del proyecto han sido:
+Algunos de los problemas que han sido necesario abordar a lo largo del desarrollo del proyecto han sido:
 
 + Dificultad de acceso a datos fiables y completos.
 + Limitación temporal y geográfica de los mismos.
@@ -51,5 +51,5 @@ Estos puntos han hecho necesario abordar técnicas de:
 + Balanceo del número de muestras de cara al entrenamiento de algoritmos: SMOTE, sobremuestreo, submuestreo.
 + Tipos de algoritmos a emplear.
 + Creación de funciones de particionado ad-Hoc.
-+ Decisión sobre métricas válidas debido al desbalance de datos, etc.</div>''', unsafe_allow_html=True)
++ Decisión sobre métricas válidas debido al desbalance de datos, etc.''')
 
