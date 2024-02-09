53c4dfcad58c1714b494e1479d047777f058e4a9
DavidG28 <131462196+DavidG28@users.noreply.github.com>
2024-02-09 19:01:17 +0100

    Update 5_Resultados.py

--- app/pages/5_Resultados.py
+++ app/pages/5_Resultados.py
@@ -49,48 +49,69 @@ Para cada uno de los mejores modelos, las matrices de confusión obtenidas han s
     with col3:
         st.image('app/graficas/knn_SVM_BAL.png')
 
-    st.markdown('''En ellas se puede ver claramente el trade-off entre fiabilidad a la hora de no pasar por alto ningún posible indicador de posible crisis futura (verdadero positivo en este caso), frente al nivel de falsos positivos generados.
+    st.markdown('''<div style="text-align: justify;">
+    
+En ellas se puede ver claramente el trade-off entre fiabilidad a la hora de no pasar por alto ningún posible indicador de posible crisis futura (verdadero positivo en este caso), frente al nivel de falsos positivos generados.
 
 Como se puede observar, el primer y tercer modelo aciertan todas las muestras de “pre-crisis” de la partición de test, mientras que el segundo falla en ocho de los casos. Sin embargo el segundo modelo tiene un nivel de falsos positivos bastante menor que los otros dos casos.
 
-En este caso coincidiendo con el mayor valor de la métrica seleccionada, consideraremos el primer modelo como el mejor de los tres.''')
+En este caso coincidiendo con el mayor valor de la métrica seleccionada, consideraremos el primer modelo como el mejor de los tres.</div>''', unsafe_allow_html=True)
 
 with tab3:
     st.markdown('''## **:orange[Predicciones]**
-Si nos centramos en dicho modelo, podemos revisar en qué muestras se han producido los errores y aciertos (los años ausentes corresponden a aquellos asociados a crisis o post-crisis, que fueron eliminados al inicio para evitar la introducción de sesgo):''')
+<div style="text-align: justify;">
+
+Si nos centramos en dicho modelo, podemos revisar en qué muestras se han producido los errores y aciertos (los años ausentes corresponden a aquellos asociados a crisis o post-crisis, que fueron eliminados al inicio para evitar la introducción de sesgo):</div>''', unsafe_allow_html=True)
 
     col1, col2, col3 = st.columns(3)
     with col2:
         st.image('app/graficas/errores_prediccion.png')
     
-    st.markdown('''Como se puede observar muchos errores se localizan en el año 2020, año de la pandemia de COVID-19 y por tanto año extremadamente excepcional en muchos aspectos y entre ellos los económicos, por lo que tiene sentido que el modelo pueda detectar patrones de futura crisis. Dado que en el conjunto de datos de trabajo no hay información para los años posteriores, todas las muestras correspondientes a 2020 aparecen marcadas como “no crisis”, pero realmente esta catalogación es cuestionable, dado que sabemos que a nivel económico los años post-pandemia podrían considerarse como años de crisis económica, con lo que la predicción del modelo podría considerarse correcta.''')
+    st.markdown('''<div style="text-align: justify;">
+    
+Como se puede observar muchos errores se localizan en el año 2020, año de la pandemia de COVID-19 y por tanto año extremadamente excepcional en muchos aspectos y entre ellos los económicos, por lo que tiene sentido que el modelo pueda detectar patrones de futura crisis. Dado que en el conjunto de datos de trabajo no hay información para los años posteriores, todas las muestras correspondientes a 2020 aparecen marcadas como “no crisis”, pero realmente esta catalogación es cuestionable, dado que sabemos que a nivel económico los años post-pandemia podrían considerarse como años de crisis económica, con lo que la predicción del modelo podría considerarse correcta.</div>''', unsafe_allow_html=True)
 
 
 
 with tab4:    
     st.markdown('''## **:orange[Interpretabilidad]**
+<div style="text-align: justify;">
     
 Una de las ventajas del modelo de Regresión Logística es su interpretabilidad, ya que evaluando los coeficientes asociados a la función logística podemos determinar qué atributos son más o menos importantes y con qué tipo de relación (directa o inversa). 
 
-Vamos a revisar si efectivamente los coeficientes asociados a cada uno de los atributos discriminativos (ordenados de mayor a menor importancia), concuerdan en su tipo de relación con el marco teórico expuesto:''')
+Vamos a revisar si efectivamente los coeficientes asociados a cada uno de los atributos discriminativos (ordenados de mayor a menor importancia), concuerdan en su tipo de relación con el marco teórico expuesto:</div>''', unsafe_allow_html=True)
 
     with st.expander('+ :orange[Diferencial relativo del Índice de Precios al Consumo (coeficiente=1,55):]'):
-        st.markdown('Como vemos coincide con lo expresado en el apartado de marco teórico acerca de que escenarios de alta/hiper-inflación pueden constituir un indicador de posible crisis financiera.')
+        st.markdown('''<div style="text-align: justify;">
+        
+    Como vemos coincide con lo expresado en el apartado de marco teórico acerca de que escenarios de alta/hiper-inflación pueden constituir un indicador de posible crisis financiera.</div>''', unsafe_allow_html=True)
 
     with st.expander('+ :orange[Diferencial absoluto del estado de Cuenta Corriente del país escalado por el Producto Interior Bruto (coeficiente=-0,86):]'):
-        st.markdown('De igual forma, a peor estado de la cuenta corriente del país, mayor indicador de problemas de salud financiera.')
+        st.markdown('''<div style="text-align: justify;">
+        
+    De igual forma, a peor estado de la cuenta corriente del país, mayor indicador de problemas de salud financiera.</div>''', unsafe_allow_html=True)
 
     with st.expander('+ :orange[Ratio Tipos de Interes a Largo Plazo vs Corto Plazo (coeficiente=-0,43):]'):
-        st.markdown('El signo negativo representaría una situación en la que los tipos de interés a corto plazo son superiores a los tipos a largo plazo y por tanto podrían indicar problemas económicos actuales.')
+        st.markdown('''<div style="text-align: justify;">
+        
+    El signo negativo representaría una situación en la que los tipos de interés a corto plazo son superiores a los tipos a largo plazo y por tanto podrían indicar problemas económicos actuales.</div>''', unsafe_allow_html=True)
 
     with st.expander('+ :orange[Aumento de Crédito Global (coeficiente=0,34):]'):
-        st.markdown('Coincidiría con el hecho de que mayores niveles de crédito indican mayor necesidad de endeudamiento y por tanto menor fortaleza económica.')
+        st.markdown('''<div style="text-align: justify;">
+        
+    Coincidiría con el hecho de que mayores niveles de crédito indican mayor necesidad de endeudamiento y por tanto menor fortaleza económica.</div>''', unsafe_allow_html=True)
 
     with st.expander('+ :orange[Diferencial absoluto del Volumen de Crédito al sector privado escalado por el Producto Interior Bruto (coeficiente=0,24):]'):
-        st.markdown('Al igual que en el punto anterior, mayores niveles de crédito indican mayor necesidad de endeudamiento y por tanto menor fortaleza económica.')
+        st.markdown('''<div style="text-align: justify;">
+        
+    Al igual que en el punto anterior, mayores niveles de crédito indican mayor necesidad de endeudamiento y por tanto menor fortaleza económica.</div>''', unsafe_allow_html=True)
 
     with st.expander('+ :orange[Curva de Rendimiento Global (Global Yield Curve) (coeficiente=-0,15):]'):
-        st.markdown('Según los expertos, una pendiente plana o descendente de la curva de rendimiento es posible indicador de recesión, lo cual coincide con el signo del coeficiente obtenido.')
+        st.markdown('''<div style="text-align: justify;">
+        
+    Según los expertos, una pendiente plana o descendente de la curva de rendimiento es posible indicador de recesión, lo cual coincide con el signo del coeficiente obtenido.</div>''', unsafe_allow_html=True)
 
     with st.expander('+ :orange[Diferencial absoluto del ratio Volumen de Crédito al sector privado - Tipo de Interés a largo plazo (coeficiente=-0,03):]'):
-        st.markdown('Sería indicativo de reducciones en el volumen de crédito disponible para el sector privado por parte del sector bancario, lo cual coincidiría con un posible indicativo de recesión económica.')
+        st.markdown('''<div style="text-align: justify;">
+    
+    Sería indicativo de reducciones en el volumen de crédito disponible para el sector privado por parte del sector bancario, lo cual coincidiría con un posible indicativo de recesión económica.</div>''', unsafe_allow_html=True)
