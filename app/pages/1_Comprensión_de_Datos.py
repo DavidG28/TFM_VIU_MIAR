0f1fb40a1908fe5e53002314aec0d044bcceea3c
DavidG28 <131462196+DavidG28@users.noreply.github.com>
2024-02-09 18:39:09 +0100

    Update 1_Comprensión_de_Datos.py

--- app/pages/1_Comprensión_de_Datos.py
+++ app/pages/1_Comprensión_de_Datos.py
@@ -52,7 +52,7 @@ with tab2:
              ' (', filtered_df['country'].nunique(),
              ' de ', data['country'].nunique(), ' países)')
     st.write('&emsp;Número de Atributos: ', filtered_df.shape[1] - 2)
-    st.write('&emsp;Balance de Clases:&emsp;', 'No Crisis: ', round(filtered_df[filtered_df['crisisJST'] == 0].shape[0] / filtered_df.shape[0] * 100, 2), '%&emsp;Crisis: ', round(filtered_df[filtered_df['crisisJST'] == 1].shape[0] / filtered_df.shape[0] * 100, 2), '%', unsafe_allow_html=True)
+    st.write('&emsp;Balance de Clases (crisisJST):&emsp;', 'No Crisis: ', round(filtered_df[filtered_df['crisisJST'] == 0].shape[0] / filtered_df.shape[0] * 100, 2), '%&emsp;Crisis: ', round(filtered_df[filtered_df['crisisJST'] == 1].shape[0] / filtered_df.shape[0] * 100, 2), '%', unsafe_allow_html=True)
     
     num_ausentes = filtered_df.isnull().sum()
     porcentaje = round(num_ausentes / filtered_df.shape[0] * 100, 2)
@@ -99,57 +99,57 @@ A partir de los datos anteriores se procede a la elaboración de los siguientes 
     
 A modo de ejemplo: el precio de la vivienda a lo largo del tiempo ha ido creciendo, pero ¿podríamos decir de forma categórica si el precio actual es alto o no sin tomar ningún otro indicador como referencia? En principio sería complicado, pero si por ejemplo calculamos un crecimiento interanual de dicho precio en un 100%, sí podríamos indicar que se ha producido un acusado aumento de los precios.</div>''', unsafe_allow_html=True)
     
-    with st.expander('+ :orange[Diferencial relativo del Índice de Precios al Consumo] (cpi_dif_por2)'):
+    with st.expander('+ :orange[Diferencial relativo del Índice de Precios al Consumo] - (cpi_dif_por2)'):
         st.markdown('''<div style="text-align: justify;">
         
 Representa el cambio relativo entre el valor actual del Índice de Precios al Consumo (IPC o Consumer Prices Index CPI en inglés), y su valor anterior (dos años antes en nuestro caso). El motivo de selección de este parámetro se debe a que como hemos visto es frecuente que en situaciones de crisis financiera y situaciones de debilidad económica se produzcan escenarios de hiper-inflación que en muchas ocasiones se unen a escenarios de devaluación de la moneda nacional. Este tipo de escenarios de inflación elevada e hiper-inflación, podrían ser detectados mediante este parámetro ya que presentarían valores elevados del mismo.''', unsafe_allow_html=True)
         
-    with st.expander('+ :orange[Diferencial absoluto del Agregado Monetario Amplio (M3) escalado por el Producto Interior Bruto del país] (money_pib_dif_abs2)'):
+    with st.expander('+ :orange[Diferencial absoluto del Agregado Monetario Amplio (M3) escalado por el Producto Interior Bruto del país] - (money_pib_dif_abs2)'):
         st.markdown('''<div style="text-align: justify;">
     
 En este caso se calculará la diferencia absoluta a dos años de dicho indicador escalado previamente por el Producto Interior Bruto. Según define el Banco Central Europeo (BCE), el Agregado Monetario Amplio integra entre otros la cantidad de moneda en circulación, los depósitos a la vista y depósitos a plazo. Según los expertos se trata de un indicador que también permite anticipar posibles escenarios de presión inflacionista y por tanto de problemas de salud financiera del país.''', unsafe_allow_html=True)
         
-    with st.expander('+ :orange[Diferencial relativo del Consumo Real per Cápita] (rconsbarro_dif_por2)'):
+    with st.expander('+ :orange[Diferencial relativo del Consumo Real per Cápita] - (rconsbarro_dif_por2)'):
         st.markdown('''<div style="text-align: justify;">
     
 De nuevo calcularemos el cambio relativo del indicador a dos años. Este indicador representa el volumen de bienes y servicios consumidos por persona y año y se considera como un buen indicador del nivel de salud de la economía.''', unsafe_allow_html=True)
         
-    with st.expander('+ :orange[Diferencial absoluto del volumen de Deuda Pública escalado por el Producto Interior Bruto] (pdebt_pib_dif_abs2)'):
+    with st.expander('+ :orange[Diferencial absoluto del volumen de Deuda Pública escalado por el Producto Interior Bruto] - (pdebt_pib_dif_abs2)'):
         st.markdown('''<div style="text-align: justify;">
     
 Como en casos anteriores calcularemos el cambio absoluto del indicador a dos años, indicador que como hemos visto puede ser signo de situaciones financieras adversas dado que en las crisis financieras los países pueden requerir aumentar su nivel de deuda acudiendo a financiación externa.''', unsafe_allow_html=True)
         
-    with st.expander('+ :orange[Diferencial absoluto del volumen de Inversión escalado por el Producto Interior Bruto] (inv_pib_dif_abs2)'):
+    with st.expander('+ :orange[Diferencial absoluto del volumen de Inversión escalado por el Producto Interior Bruto] - (inv_pib_dif_abs2)'):
         st.markdown('''<div style="text-align: justify;">
     
 De nuevo diferencial calculado a dos años. Como hemos visto uno de los posibles motivos desencadenantes de situaciones de crisis es la pérdida de confianza de los inversores en el país con el correspondiente descenso del volumen de inversión.''', unsafe_allow_html=True)
         
-    with st.expander('+ :orange[Diferencial absoluto del estado de Cuenta Corriente del país escalado por el Producto Interior Bruto] (ca_pib_dif_abs2)'):
+    with st.expander('+ :orange[Diferencial absoluto del estado de Cuenta Corriente del país escalado por el Producto Interior Bruto] - (ca_pib_dif_abs2)'):
         st.markdown('''<div style="text-align: justify;">
     
 Calculado nuevamente a dos años. Indicador que refleja el volumen de transacciones comerciales y de bienes y servicios transfronterizas o cross-border. Como hemos visto, situaciones de bloqueo comercial que podrían culminar en situaciones de crisis, quedarían reflejadas en este indicador.''', unsafe_allow_html=True)
         
-    with st.expander('+ :orange[Diferencial absoluto del Volumen de Crédito al sector privado escalado por el Producto Interior Bruto] (tloans_pib_dif_abs2)'):
+    with st.expander('+ :orange[Diferencial absoluto del Volumen de Crédito al sector privado escalado por el Producto Interior Bruto] - (tloans_pib_dif_abs2)'):
         st.markdown('''<div style="text-align: justify;">
     
 Calculado a dos años. Como hemos visto volúmenes de crédito elevados podrían estar relacionados con la aparición de “burbújas” financieras que podrían desencadenar finalmente situaciones de crisis.''', unsafe_allow_html=True)
         
-    with st.expander('+ :orange[Diferencial absoluto del ratio Volumen de Crédito al sector privado - Tipo de Interés a largo plazo] (tdbtserv_pib_dif_abs2)'):
+    with st.expander('+ :orange[Diferencial absoluto del ratio Volumen de Crédito al sector privado - Tipo de Interés a largo plazo] - (tdbtserv_pib_dif_abs2)'):
         st.markdown('''<div style="text-align: justify;">
         
 Nuevamente calculado a un plazo de dos años. Elevados volúmenes de crédito con expectativas de altos tipos de interés a largo plazo pueden indicar problemas futuros de salud financiera del país.''', unsafe_allow_html=True)
         
-    with st.expander('+ :orange[Aumento de Crédito Global] (global_loan2)'):
+    with st.expander('+ :orange[Aumento de Crédito Global] - (global_loan2)'):
         st.markdown('''<div style="text-align: justify;">
         
 Cálculo a dos años. En este caso se compara la evolución del nivel de crédito mundial excluyendo el país bajo estudio.''', unsafe_allow_html=True)
         
-    with st.expander('+ :orange[Curva de Rendimiento Global (Global Yield Curve)] (global_drate)'):
+    with st.expander('+ :orange[Curva de Rendimiento Global (Global Yield Curve)] - (global_drate)'):
         st.markdown('''<div style="text-align: justify;">
         
 La curva refleja la variación del rendimiento (tipo de interés), de la deuda pública emitida (bonos del estado), de manera global frente al país bajo estudio. Países en situación de mala salud financiera tendrán mayores problemas de financiación y deberán proporcionar mayores tipos de interés a los inversores para acceder a financiación externa.''', unsafe_allow_html=True)
         
-    with st.expander('+ :orange[Ratio Tipos de Interes a Largo Plazo vs Corto Plazo] (drate)'):
+    with st.expander('+ :orange[Ratio Tipos de Interes a Largo Plazo vs Corto Plazo] - (drate)'):
         st.markdown('''<div style="text-align: justify;">
         
 Aumentos en los tipos de interés podrían indicar restricciones de acceso a la financiación privada, lo que podría ser indicativo de problemas de salud financiera.''', unsafe_allow_html=True)
@@ -165,7 +165,7 @@ with tab4:
     
     # Mostramos métricas de interés
     st.write('&emsp;Número de Atributos: ', data_final.shape[1] - 2, unsafe_allow_html=True)
-    st.write('&emsp;Balance de Clases:&emsp;', 'No Crisis: ', round(data_final[data_final['crisisJST'] == 0].shape[0] / data_final.shape[0] * 100, 2), '%&emsp;Pre-Crisis: ', round(data_final[data_final['crisisJST'] == 1].shape[0] / data_final.shape[0] * 100, 2), '%', unsafe_allow_html=True)
+    st.write('&emsp;Balance de Clases (crisisJST):&emsp;', 'No Crisis: ', round(data_final[data_final['crisisJST'] == 0].shape[0] / data_final.shape[0] * 100, 2), '%&emsp;Pre-Crisis: ', round(data_final[data_final['crisisJST'] == 1].shape[0] / data_final.shape[0] * 100, 2), '%', unsafe_allow_html=True)
     col1, col2, col3 = st.columns(3)
     with col2:
         st.image('app/graficas/comprension_final_instancias_clase.png')
