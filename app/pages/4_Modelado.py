2600fd2fd084e5c4424f80b2006edc52c3c2fcfa
DavidG28 <131462196+DavidG28@users.noreply.github.com>
2024-02-11 17:49:16 +0100

    Update 4_Modelado.py

--- app/pages/4_Modelado.py
+++ app/pages/4_Modelado.py
@@ -57,9 +57,9 @@ with tab2:
     st.markdown('''## **:orange[Particionado Interno]**
 <div style="text-align: justify;">
     
-Como ocurría durante la realización del particionado externo (Hold-out), durante la creación de los folds para la realización de cross-validation o de particionado interno en particiones de entrenamiento y validación, hay que asegurar que no se produce fuga de datos, es decir, garantizar que en las particiones de entrenamiento no existen datos asociados a instantes posteriores a los contenidos en las particiones de validación. Además dado el gran desbalance de muestras entre clases, hay que garantizar también la existencia de muestras de ambas clases en todas las particiones.
+Como ocurría durante la realización del particionado externo (Hold-out), durante la creación de los folds para la realización de cross-validation o de particionado interno en particiones de entrenamiento y validación, **:orange[hay que asegurar que no se produce fuga de datos, es decir, garantizar que en las particiones de entrenamiento no existen datos asociados a instantes posteriores a los contenidos en las particiones de validación. Además dado el gran desbalance de muestras entre clases, hay que garantizar también la existencia de muestras de ambas clases en todas las particiones]**.
 
-Por este motivo se ha creado una función customizada para la creación de las distintas particiones, de manera que se asegure por un lado la coherencia temporal (no existencia de fuga de datos), y por otro la existencia de instancias de ambas clases, imitando para ello el comportamiento de “TimeSeriesSplit. Para ello se calcula un reparto equitativo de muestras minoritarias y se van creando las particiones de manera secuencial. A continuación se puede observar gráficamente un ejemplo para la creación de cinco folds:</div>''', unsafe_allow_html=True)
+Por este motivo **:orange[se ha creado una función customizada para la creación de las distintas particiones, de manera que se asegure por un lado la coherencia temporal (no existencia de fuga de datos), y por otro la existencia de instancias de ambas clases, imitando para ello el comportamiento de "TimeSeriesSplit"]**. Para ello se calcula un reparto equitativo de muestras minoritarias y se van creando las particiones de manera secuencial. A continuación se puede observar gráficamente un ejemplo para la creación de cinco folds:</div>''', unsafe_allow_html=True)
     col1, col2, col3 = st.columns(3)
     with col2:
         st.image('app/imagenes/folds.png')
@@ -68,31 +68,31 @@ with tab3:
     st.markdown('''## **:orange[Técnicas de Balanceo de Datos]**
 <div style="text-align: justify;">
 
-Dado el desbalance en lo que a número de muestras asociadas a cada clase respecta, es necesario aplicar algún tipo de estrategia para conseguir un número equitativo de muestras en cada clase. Para ello se han aplicado las siguientes metodologías a cada uno de los algoritmos indicados en el punto anterior:</div>''', unsafe_allow_html=True)
+Dado el desbalance en lo que a número de muestras asociadas a cada clase respecta, **:orange[es necesario aplicar algún tipo de estrategia para conseguir un número equitativo de muestras en cada clase]**. Para ello se han aplicado las siguientes metodologías a cada uno de los algoritmos indicados en el punto anterior:</div>''', unsafe_allow_html=True)
     with st.container():
         col1, col2, col3, col4 = st.columns(4)
         
         with col1:
             st.write('''<div style="text-align: justify;">
         
-**:orange[SMOTE (Syntetic Minority Oversampling Technique):]** se trata de una técnica que se encarga de crear muestras artificiales de la clase minoritaria a partir de las muestras existentes en el conjunto de datos que toma como origen.
+**:orange[SMOTE (Syntetic Minority Oversampling Technique):]** se trata de una técnica que se encarga de **crear muestras artificiales de la clase minoritaria** a partir de las muestras existentes en el conjunto de datos que toma como origen.
             
 El funcionamiento se puede ver de manera gráfica en la siguiente imagen:</div>''', unsafe_allow_html=True)
     
         with col2:
             st.write('''<div style="text-align: justify;">
             
-**:orange[Random Oversampling:]** se trata de nuevo de una técnica de “sobremuestreo” u “oversampling” de la clase minoritaria, pero en este caso no se crean muestras artificiales, sino que de la partición original se eligen de forma aleatoria muestras existentes correspondientes a dicha clase que se incluyen repetidas veces en la partición final para conseguir el deseado balance en el número de muestras.</div>''', unsafe_allow_html=True)
+**:orange[Random Oversampling:]** se trata de nuevo de una técnica de **“sobremuestreo” u “oversampling” de la clase minoritaria**, pero en este caso no se crean muestras artificiales, sino que de la partición original se eligen de forma aleatoria muestras existentes correspondientes a dicha clase que se incluyen repetidas veces en la partición final para conseguir el deseado balance en el número de muestras.</div>''', unsafe_allow_html=True)
     
         with col3:
             st.write('''<div style="text-align: justify;">
             
-**:orange[Random Undersampling:]** a diferencia de los dos casos anteriores, esta técnica consiste en un “submuestreo” o “undersampling” de las muestras asociadas a la clase mayoritaria, manteniendo inalterado el número de muestras correspondientes a la clase minoritaria. Como consecuencia se reduce enórmemente el número de muestras útiles.</div>''', unsafe_allow_html=True)
+**:orange[Random Undersampling:]** a diferencia de los dos casos anteriores, esta técnica consiste en un **“submuestreo” o “undersampling” de las muestras asociadas a la clase mayoritaria**, manteniendo inalterado el número de muestras correspondientes a la clase minoritaria. Como consecuencia se reduce enórmemente el número de muestras útiles.</div>''', unsafe_allow_html=True)
         
         with col4:
             st.write('''<div style="text-align: justify;">
             
-**:orange[Balance de pesos:]** se ha hecho uso del parámetro “class_weight”, que permite calibrar los pesos asociados a cada clase, de forma que el algoritmo pondere de manera diferente los errores cometidos con las muestras de cada tipo de clase, penalizando en mayor medida los errores cometidos con muestras de la clase minoritaria frente a los errores cometidos con muestras de la clase mayoritaria.</div>''', unsafe_allow_html=True)
+**:orange[Balance de pesos:]** se ha hecho uso del parámetro “class_weight”, que permite **calibrar los pesos asociados a cada clase, de forma que el algoritmo pondere de manera diferente los errores cometidos con las muestras de cada tipo de clase**, penalizando en mayor medida los errores cometidos con muestras de la clase minoritaria frente a los errores cometidos con muestras de la clase mayoritaria.</div>''', unsafe_allow_html=True)
     
     with st.container():
         col1, col2, col3, col4 = st.columns(4)
@@ -114,7 +114,7 @@ with tab4:
     
 Un punto importante a destacar y en el que debemos centrarnos, es el referido a cuándo se debe realizar el proceso de balanceo de los datos en el caso de usar cross-validation. Si dicho proceso se aplica directamente sobre el conjunto de datos de entrenamiento sobre el cual a su vez se aplicará posteriormente una función para el reparto en folds, este proceso no garantiza que en las particiones finales de entrenamiento se mantenga dicho reparto equilibrado de muestras, por lo que no habría sido de utilidad.
 
-Para asegurar que se mantiene el buscado reparto equitativo de muestras en las particiones de entrenamiento, el proceso de balanceo debe realizarse directamente sobre la partición de entrenamiento de cada fold. Para ello se ha hecho uso de la librería “Imbalanced-learn”, en concreto de la utilidad “make_pipeline” que permite crear un estimador que sirva de entrada al proceso de entrenamiento concatenando para ello transformaciones, muestreos y el algoritmo concreto a usar. En este caso sólo se han concatenado el proceso de balanceo de datos (usando para ello cada una de las estrategias indicadas anteriormente), y el algoritmo de clasificación a usar.</div>''', unsafe_allow_html=True)
+**:orange[Para asegurar que se mantiene el buscado reparto equitativo de muestras en las particiones de entrenamiento, el proceso de balanceo debe realizarse directamente sobre la partición de entrenamiento de cada fold]**. Para ello se ha hecho uso de la librería “Imbalanced-learn”, en concreto de la utilidad “make_pipeline” que permite crear un estimador que sirva de entrada al proceso de entrenamiento concatenando para ello transformaciones, muestreos y el algoritmo concreto a usar. En este caso sólo se han concatenado el proceso de balanceo de datos (usando para ello cada una de las estrategias indicadas anteriormente), y el algoritmo de clasificación a usar.</div>''', unsafe_allow_html=True)
 
     col1, col2, col3 = st.columns(3)
     with col2:
@@ -124,13 +124,13 @@ with tab5:
     st.markdown('''## **:orange[Métricas de Evaluación]**
 <div style="text-align: justify;">
     
-En casos como el presente u otros parecidos (como pueden ser detección de fraude, anomalías, etc.), donde el volumen de muestras de la clase minoritaria es muy bajo comparado con el de muestras de la clase mayoritaria, lo que se suele buscar es una métrica que maximice el número de verdaderos positivos, pero manteniendo un equilibrio con el número de falsos positivos, los cuales estarían representando falsas alarmas que deberían ser revisadas con los posibles costes asociados que ésto podría suponer.
+**:orange[En casos como el presente]** u otros parecidos (como pueden ser detección de fraude, anomalías, etc.), donde el volumen de muestras de la clase minoritaria es muy bajo comparado con el de muestras de la clase mayoritaria, lo que **:orange[se suele buscar es una métrica que maximice el número de verdaderos positivos, pero manteniendo un equilibrio con el número de falsos positivos]**, los cuales estarían representando falsas alarmas que deberían ser revisadas con los posibles costes asociados que ésto podría suponer.
 
-Una métrica que nos permite evaluar dicho equilibrio es “sklearn.metrics.average_precision_score”, que calcula una aproximación del área bajo la curva precisión-sensibilidad (Area Under the Precision-Recall Curve, AUPRC en inglés), métrica que en casos de datos desbalanceados suele considerarse más estable que el área bajo la curva Característica Operativa del Receptor (Area Under the Receiver Operating Characteristic Curve, ROC-AUC), que suele usarse en clasificación binaria de conjuntos de datos balanceados para evaluar la capacidad discriminativa del modelo entre clase positiva y negativa en función del umbral de decisión establecido.
+**:orange[Una métrica que nos permite evaluar dicho equilibrio es “sklearn.metrics.average_precision_score”]**, que calcula una aproximación del área bajo la curva Precisión-Sensibilidad, métrica que en casos de datos desbalanceados suele considerarse más estable que el área bajo la curva ROC, que suele usarse en clasificación binaria de conjuntos de datos balanceados para evaluar la capacidad discriminativa del modelo entre clases en función del umbral de decisión establecido.
 
-También puede ocurrir que el nivel de penalización considerado para un falso positivo y un falso negativo sean diferentes, es decir, puede ocurrir que un falso negativo sea muy grave, pero no lo sea un falso positivo o falsa alarma, en cuyo caso nos convendría usar otras métricas para potenciar minimizar por ejemplo la sensibilidad o recall del modelo para la clase minoritaria o modificar el umbral de decisión.
+**:orange[Puede ocurrir que el nivel de penalización considerado para un falso positivo y un falso negativo sean diferentes]**, es decir, puede ocurrir que un falso negativo sea muy grave, pero no lo sea un falso positivo o falsa alarma, en cuyo caso nos **:orange[convendría usar otras métricas para potenciar minimizar por ejemplo la sensibilidad o recall del modelo para la clase minoritaria o modificar el umbral de decisión]**.
 
-En este caso consideraremos que ambas situaciones (falso positivo y falso negativo), son igualmente perjudiciales y usaremos como parámetro de evaluación para GridSearchCV el citado “average_precision_score”, para lo cual crearemos nuestra propia métrica haciendo uso de “sklearn.metrics.make_scorer”.</div>''', unsafe_allow_html=True)
+En este caso **:orange[consideraremos ambas situaciones como igualmente perjudiciales y usaremos como parámetro de evaluación para GridSearchCV el citado “average_precision_score”}**, para lo cual crearemos nuestra propia métrica haciendo uso de “sklearn.metrics.make_scorer”.</div>''', unsafe_allow_html=True)
 
     col1, col2, col3, col4 = st.columns(4)
     with col2:
