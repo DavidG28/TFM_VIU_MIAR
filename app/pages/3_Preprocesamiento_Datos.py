e03cab2aaf463e9f3fd7c5b6cd5e9e7b1c5aad4d
DavidG28 <131462196+DavidG28@users.noreply.github.com>
2024-02-11 17:22:52 +0100

    Update 3_Preprocesamiento_Datos.py

--- app/pages/3_Preprocesamiento_Datos.py
+++ app/pages/3_Preprocesamiento_Datos.py
@@ -171,7 +171,7 @@ with tab2:
     st.markdown('''#### **Eliminación de Atributos por Missing Value Ratio**
 <div style="text-align: justify;">
 
-Dentro de este punto, el primer paso será eliminar aquellas características que superen un Missing Value Ratio del 30%, dado que si se supera dicho umbral significaría que la información aportada por dicho atributo sería muy escasa:</div>''', unsafe_allow_html=True)
+Dentro de este punto, el primer paso será **:orange[eliminar aquellas características que superen un Missing Value Ratio del 30%, dado que si se supera dicho umbral significaría que la información aportada por dicho atributo sería muy escasa]**:</div>''', unsafe_allow_html=True)
 
     atributos_eliminados, max_porcentaje, min_porcentaje = eliminacion_missing_value_ratio(X_train_std, X_test_std)
     # atributos_eliminados = 53
@@ -184,7 +184,7 @@ Dentro de este punto, el primer paso será eliminar aquellas características qu
     st.markdown('''#### **Estrategias de Imputación de Valores Ausentes**
 <div style="text-align: justify;">
 
-De cara a poder realizar una comparativa posterior en base a las métricas de los distintos algoritmos, se han planteado tres estrategias de imputación de valores ausentes:</div>''', unsafe_allow_html=True)
+**:orange[De cara a poder realizar una comparativa]** posterior en base a las métricas de los distintos algoritmos, **:orange[se han planteado tres estrategias de imputación de valores ausentes]**:</div>''', unsafe_allow_html=True)
     col1, col2, col3 = st.columns(3)
     
     with col1:
@@ -206,13 +206,13 @@ with tab3:
     st.markdown('''## **:orange[Eliminación de Outliers]**
 <div style="text-align: justify;">
     
-Aunque por lo general como parte del proceso de procesamiento de los datos siempre se suele aplicar una etapa de eliminación de valores atípicos o outliers, en el presente caso se ha optado por no aplicarla dado que podría ser contraproducente y provocar pérdidas de información o patrones valiosos en lugar de las mejoras esperadas.
+Aunque por lo general como parte del proceso de procesamiento de los datos siempre se suele aplicar una etapa de eliminación de valores atípicos o outliers, **:orange[en el presente caso se ha optado por no aplicarla dado que podría ser contraproducente]** y provocar pérdidas de información o patrones valiosos en lugar de las mejoras esperadas.
 
 Los motivos principales de este comportamiento negativo serían dos:
 
 * Por un lado hay que tener en cuenta que se está trabajando con datos correspondientes a distintos países y por tanto los rangos asociados a los datos macroeconómicos (a pesar de haber usado parámetros estandarizadores), podrían ser diferentes sin que eso supusiera la existencia de outliers. En este caso al menos deberíamos buscar valores atípicos limitando los datos a los correspondientes a cada país en lugar de hacer una búsqueda sobre el conjunto completo de datos disponibles.
 
-* Sin embargo el punto más grave de proceder con la realización de una eliminación de outliers sería el siguiente: como sabemos uno de los posibles indicadores predictivos de una posible situación futura de crisis financiera es un escenario de hiper-inflación. Este escenario se traduciría en un valor atípico del indicador macroeconómico del Índice de Precios al Consumo. Si como parte de nuestro proceso de procesamiento de los datos eliminamos estos valores atípicos, estaríamos evitando que el modelo predictivo pudiera detectar este tipo de patrones para la predicción de las futuras crisis financieras. Ésto mismos podría ocurrir con el resto de atributos empleados.</div>''', unsafe_allow_html=True)
+* Sin embargo el punto más grave de proceder con la realización de una eliminación de outliers sería el siguiente: como sabemos uno de los posibles indicadores predictivos de **:orange[una posible situación futura de crisis financiera es un escenario de hiper-inflación. Este escenario se traduciría en un valor atípico del indicador macroeconómico del Índice de Precios al Consumo. Si como parte de nuestro proceso de procesamiento de los datos eliminamos estos valores atípicos, estaríamos evitando que el modelo predictivo pudiera detectar este tipo de patrones]** para la predicción de las futuras crisis financieras. Ésto mismos podría ocurrir con el resto de atributos empleados.</div>''', unsafe_allow_html=True)
 
 with tab4:
     st.write('## **:orange[Selección de Atributos]**')
@@ -220,11 +220,11 @@ with tab4:
     st.write('#### **Atributos Discriminativos:**')
     st.markdown('''<div style="text-align: justify;">
     
-El siguiente proceso a realizar se centra en eliminar aquellos atributos que no sean discriminativos con respecto de la clase. Para ello se han seguido los siguientes pasos:
+El siguiente proceso a realizar se centra en **:orange[eliminar aquellos atributos que no sean discriminativos con respecto de la clase]**. Para ello se han seguido los siguientes pasos:
 
-+ Para cada atributo se realiza un estudio para determinar si sigue o no una distribución normal mediante la prueba de Kolmogorov-Smirnov.
-+ En caso de no seguir una distribución normal, se realiza una comparativa de las medianas de los atributos para las muestras asociadas a cada clase (prueba U de Mann-Whitney).
-+ En caso de seguir una distribución normal, se realiza una comparativa de las medias de los atributos para las muestras asociadas a cada clase (prueba T de Student).</div>''', unsafe_allow_html=True)
++ Para cada atributo se realiza un estudio para **:orange[determinar si sigue o no una distribución normal mediante la prueba de Kolmogorov-Smirnov]**.
++ En caso de **:orange[no seguir una distribución normal, se realiza una comparativa de las medianas]** de los atributos para las muestras asociadas a cada clase **:orange[(prueba U de Mann-Whitney)]**.
++ En caso de seguir una **:orange[distribución normal, se realiza una comparativa de las medias]** de los atributos para las muestras asociadas a cada clase **:orange[(prueba T de Student)]**.</div>''', unsafe_allow_html=True)
 
     with open('preprocesadores/nuevos_atributos.pkl', 'rb') as f:
         nuevos_atributos = pickle.load(f)
